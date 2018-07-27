fe091d510e31770ba959b7c5708ef468ecfc9aed
BoDaly <31381847+BoDaly@users.noreply.github.com>
2018-07-27 15:15:04 -0600

    Update app.js

--- ub_test/app.js
+++ ub_test/app.js
@@ -1,5 +1,5 @@
-import Vue from 'https://bodaly.github.io/ub_test/node_modules/vue/dist/vue.js';
-import Builder from 'https://bodaly.github.io/ub_test/builder.vue';
+import Vue from 'vue/dist/vue.js';
+import Builder from 'builder.vue';
 
 let app = new Vue({
   el:'#app',
