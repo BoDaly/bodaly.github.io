07a92ac142f7a6608c9654ac7d3386c91c9881bc
BoDaly <31381847+BoDaly@users.noreply.github.com>
2018-07-27 15:21:00 -0600

    Update app.js

--- ub_test/app.js
+++ ub_test/app.js
@@ -1,5 +1,5 @@
-const Vue = import('node_modules/vue/dist/vue.js');
-const Builder = import('builder.vue');
+const Vue = import('/node_modules/vue/dist/vue.js');
+const Builder = import('/builder.vue');
 
 let app = new Vue({
   el:'#app',
