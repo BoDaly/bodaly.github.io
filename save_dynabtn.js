31409e080c68407d1061b3a71855e265ae68b3f8
BoDaly <31381847+BoDaly@users.noreply.github.com>
2018-12-19 12:49:06 -0700

    Create save_dynabtn.js

---
+++ save_dynabtn.js
@@ -0,0 +1,9 @@
+var dynamic = document.querySelector(".dynamic-button")
+var spans = dynamic.querySelectorAll("span")
+for ( let span of spans ){
+    span.setAttribute('data-skip-hko-acc', true)
+}
+var buttons = dynamic.querySelectorAll("button")
+for ( let button of buttons ){
+    button.setAttribute('data-skip-hko-acc', true)
+}
