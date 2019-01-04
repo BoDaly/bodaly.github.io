7199b27bcd5c3f240f1a0bb85e161b951cb693e8
BoDaly <31381847+BoDaly@users.noreply.github.com>
2019-01-04 15:12:13 -0700

    Update save_dynabtn.js

--- save_dynabtn.js
+++ save_dynabtn.js
@@ -1,9 +1,15 @@
+var config = { attributes: true, childList: true, subtree: true };
 var dynamic = document.querySelector(".dynamic-button")
-var spans = dynamic.querySelectorAll("span")
-for ( let span of spans ){
-    span.setAttribute('data-skip-hko-acc', true)
-}
-var buttons = dynamic.querySelectorAll("button")
-for ( let button of buttons ){
-    button.innerText = "More payment options."
-}
+var callback = function(mutationsList, observer) {
+    for(var mutation of mutationsList) {
+        if (mutation.type == 'childList') {
+            console.log('A child node has been added or removed.');
+        }
+        else if (mutation.type == 'attributes') {
+            console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        }
+    }
+};
+var observer = new MutationObserver(callback);
+
+observer.observe(targetNode, config);
